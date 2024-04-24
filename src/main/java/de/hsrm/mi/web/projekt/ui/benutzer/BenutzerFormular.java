c17d3e438ff015052f3712e2ba6d32cf9cacc859
Simon Jäckel <simon.jaeckel@student.hs-rm.de>
2024-04-24 15:46:36 +0200

    filled Benutzerformular

--- src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerFormular.java
+++ src/main/java/de/hsrm/mi/web/projekt/ui/benutzer/BenutzerFormular.java
@@ -7,6 +7,36 @@ public class BenutzerFormular {
     private String password;
     private String birthday;
 
-    
+    public String getName() {
+        return name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
 
 }
