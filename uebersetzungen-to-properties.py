781488cad15e9667924a9970a26a16f3880e8018
Simon Jäckel <simon.jaeckel@student.hs-rm.de>
2024-05-15 14:34:27 +0200

    add: finished python script

--- uebersetzungen-to-properties.py
+++ uebersetzungen-to-properties.py
@@ -3,9 +3,12 @@ with open("uebersetzungen.csv") as file:
         words = line.split(";")
         if words[0] == "propertyname":
             for i in range(len(words)-1):
+                languages = words[1:]
                 open("src/main/resources/messages_" + words[i+1].strip() + ".properties", "w")
             continue
         
-        if words[0].strip() != "#" and len(words) != 0:
-            print(words[0], len(words))
+        if not words[0].startswith("\n") and not words[0].startswith("#") and len(words) != 0:
+            for i, l in enumerate(languages):
+                f = open("src/main/resources/messages_" + l.strip() + ".properties", "a")
+                f.write(words[0] + "=" + words[i] + "\n")
 
