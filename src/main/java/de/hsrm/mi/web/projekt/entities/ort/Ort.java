f57b2f317878f5985735343f1da74c77760d46c4
Simon Jäckel <simon.jaeckel@student.hs-rm.de>
2024-07-03 21:21:20 +0200

    fixed ort delete bug

--- src/main/java/de/hsrm/mi/web/projekt/entities/ort/Ort.java
+++ src/main/java/de/hsrm/mi/web/projekt/entities/ort/Ort.java
@@ -1,13 +1,18 @@
 package de.hsrm.mi.web.projekt.entities.ort;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.springframework.data.annotation.Version;
 
+import de.hsrm.mi.web.projekt.entities.tour.Tour;
 import de.hsrm.mi.web.projekt.services.geo.GeoAdresse;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotBlank;
 
 @Entity
@@ -21,6 +26,10 @@ public class Ort implements Serializable{
     String name;
     double geobreite;
     double geolaenge;
+    @OneToMany(mappedBy="startort", cascade=CascadeType.ALL, orphanRemoval = true)
+    private Set<Tour> startTouren = new HashSet<>();
+    @OneToMany(mappedBy="zielort", cascade=CascadeType.ALL, orphanRemoval = true)
+    private Set<Tour> zielTouren = new HashSet<>();
     
     public String getName() {
         return name;
@@ -43,6 +52,23 @@ public class Ort implements Serializable{
     public long getId() {
         return this.id;
     }
+
+    public Set<Tour> getStartTouren() {
+        return this.startTouren;
+    }
+
+    public void setStartTouren(Set<Tour> t) {
+        this.startTouren = t;
+    }
+
+    public Set<Tour> getZielTouren() {
+        return this.zielTouren;
+    }
+
+    public void setZielTouren(Set<Tour> t) {
+        this.zielTouren = t;
+    }
+
     public Ort fromGeoAdresse(GeoAdresse geoAdresse) {
         this.geobreite = geoAdresse.lat();
         this.geolaenge = geoAdresse.lon();
